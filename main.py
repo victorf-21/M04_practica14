51587f47af1c6ff2fb3bb090698a8b674ed4a40d
victor <115487973+victorf-21@users.noreply.github.com>
2023-03-13 20:47:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ from B import cat as ct
 
 c1 = ct.cat("Michu","callejero","hembra","tricolor","3kg")
 c1.llamada()
+
+from B import cat as ca
+c2 = ca.cat("Anubis","Sphynx","macho","calvo","2,5kg")
+c2.llamada()
