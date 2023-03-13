881cca42b03267d0ca7cd09159ceb406eaba2b11
victor <115487973+victorf-21@users.noreply.github.com>
2023-03-13 20:52:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,11 @@ from B import cat as ca
 c2 = ca.cat("Anubis","Sphynx","macho","calvo","2,5kg")
 c2.llamada()
 
+cats_list = [u.to_dict() for u in cats]
+
+data = {"cats":cats_list}
+
+with open("json_API/cats.json", "w") as file:
+    json.dump(data, file)
+
 #Hecho por Albert
